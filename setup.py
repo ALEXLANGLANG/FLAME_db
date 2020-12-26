dcf81c76a5aa8547f35a1aefabacde7c6399eb7b
ALEXLANGLANG <sunxian1107@gmail.com>
2020-12-26 13:00:25 -0500

    optional dependenices

--- setup.py
+++ setup.py
@@ -22,6 +22,9 @@ setuptools.setup(name='flame_db',
           'pandas>=0.11.0',
           'numpy>=1.6.1'
       ],
+      extras_require={
+        "SQL":  ["mysql.connector>=2.2.9", "psycopg2>=2.8.6","pyodbc>=4.0.30"],
+    }
      long_description_content_type="text/markdown",
      classifiers=[
         "Programming Language :: Python :: 3",
