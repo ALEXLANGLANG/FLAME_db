ceb1b7f9f7cf9068fa3dfdb679e891a662ca7524
ALEXLANGLANG <sunxian1107@gmail.com>
2021-01-10 21:07:38 -0500

    s

--- tests1/flame_db_test_main2.py
+++ tests1/flame_db_test_main2.py
@@ -26,7 +26,7 @@ def check_statistics(res_post_new):
 
 p = 20
 TE = 5
-gen_data_db(n = 1000,p = 2, TE = TE)
+gen_data_db(n = 100,p = 2, TE = TE)
 data,weight_array = gen_data_db(n = 1000,p = p, TE = TE)
 holdout,weight_array = gen_data_db(n = 500,p = p, TE = TE)
 #Connect to the database
@@ -38,46 +38,46 @@ user="newuser"
 password= "sunxian123"
 conn = connect_db(database_name, user, password, host, port)
 #Insert the data into database
-insert_data_to_db("test_df100", # The name of your table containing the dataset to be matched
+insert_data_to_db("test_df200", # The name of your table containing the dataset to be matched
                     data,
                     treatment_column_name= "treated",
                     outcome_column_name= 'outcome',conn = conn)
 class TestFlame_db(unittest.TestCase):
               
-    def test_weights(self):
-        is_corrct = 1
-        try:
-            res_post_new1 = FLAME_db(input_data = "test_df100", # The name of your table containing the dataset to be matched
-                                    holdout_data = holdout, # holdout set
-                                    treatment_column_name= "treated",
-                                    outcome_column_name= 'outcome',
-                                    C = 0.1,
-                                    conn = conn,
-                                    matching_option = 0,
-                                    verbose = 3,
-                                    k = 0
-                                    )
-
-            res_post_new2 = FLAME_db(input_data = "test_df100", # The name of your table containing the dataset to be matched
-                                    holdout_data = holdout, # holdout set
-                                    treatment_column_name= "treated",
-                                    outcome_column_name= 'outcome',
-                                    C = 0.1,
-                                    conn = conn,
-                                    matching_option = 2,
-                                    adaptive_weights = False,
-                                    weight_array = weight_array,
-                                    verbose = 3,
-                                    k = 0
-                                    )
-            if check_statistics(res_post_new1) or check_statistics(res_post_new2):
-                is_corrct = 0
-            
-        except (KeyError, ValueError):
-                is_corrct = 0
-
-        self.assertEqual(1, is_corrct,
-                             msg='Error when test weights')
+#    def test_weights(self):
+#        is_corrct = 1
+#        try:
+#            res_post_new1 = FLAME_db(input_data = "test_df100", # The name of your table containing the dataset to be matched
+#                                    holdout_data = holdout, # holdout set
+#                                    treatment_column_name= "treated",
+#                                    outcome_column_name= 'outcome',
+#                                    C = 0.1,
+#                                    conn = conn,
+#                                    matching_option = 0,
+#                                    verbose = 3,
+#                                    k = 0
+#                                    )
+#
+#            res_post_new2 = FLAME_db(input_data = "test_df100", # The name of your table containing the dataset to be matched
+#                                    holdout_data = holdout, # holdout set
+#                                    treatment_column_name= "treated",
+#                                    outcome_column_name= 'outcome',
+#                                    C = 0.1,
+#                                    conn = conn,
+#                                    matching_option = 2,
+#                                    adaptive_weights = False,
+#                                    weight_array = weight_array,
+#                                    verbose = 3,
+#                                    k = 0
+#                                    )
+#            if check_statistics(res_post_new1) or check_statistics(res_post_new2):
+#                is_corrct = 0
+#
+#        except (KeyError, ValueError):
+#                is_corrct = 0
+#
+#        self.assertEqual(1, is_corrct,
+#                             msg='Error when test weights')
 
 
     def test_missing_datasets(self):
@@ -89,7 +89,7 @@ class TestFlame_db(unittest.TestCase):
             for i in range(int(m/100)):
                 for j in [0,int(n/2)]:
                     holdout_miss.iloc[i,j] = np.nan
-            res_post_new = FLAME_db(input_data = "test_df100", # The name of your table containing the dataset to be matched
+            res_post_new = FLAME_db(input_data = "test_df200", # The name of your table containing the dataset to be matched
                                     holdout_data = holdout_miss, # holdout set
                                     C = 0,
                                     conn = conn,
